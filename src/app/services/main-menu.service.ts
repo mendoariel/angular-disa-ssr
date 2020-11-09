a81ba1f1e7b943c98caed8395a7e6d624f3a8b48
mendoariel <mendoariel@hotmail.com>
2020-11-09 18:59:54 -0300

    all funciont ok with angular matarial

--- src/app/services/main-menu.service.ts
+++ src/app/services/main-menu.service.ts
@@ -14,7 +14,7 @@ export class MainMenuService {
     menu.push({name: 'Inicio', url: 'home'});
     menu.push({name: 'Nosotros', url: 'nosotros'});
     menu.push({name: 'Productos', url: 'productos'});
-    menu.push({name: 'Ofetas', url: 'ofertas'});
+    menu.push({name: 'Ofertas', url: 'ofertas'});
     menu.push({name: 'Servicios', url: 'servicios'});
     menu.push({name: 'Contactanos', url: 'contactanos'});
     //console.log(menu);
